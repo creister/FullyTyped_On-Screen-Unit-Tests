6773d49bd135a60f53cd0593517382d177c7eb22
Colin Reisterer <creisterer@projectdecibel.com>
2019-01-12 16:43:13 -0800

    Setup from "UnitTestWithoutApp"

--- OnScreenUnitTests/AppDelegate.swift
+++ OnScreenUnitTests/AppDelegate.swift
@@ -15,7 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    #if DEBUG
+    // Short-circuit starting app if running unit tests
+    let isUnitTesting = ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+    guard !isUnitTesting else {
+      return true
+    }
+    #endif
+
     // Override point for customization after application launch.
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window!.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
+    window!.makeKeyAndVisible()
     return true
   }
 
