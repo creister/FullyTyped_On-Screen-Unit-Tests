6c99766f42bfd41f122e76cf3c545e7a1446f61a
Colin Reisterer <creisterer@projectdecibel.com>
2019-01-12 16:36:00 -0800

    Xcode new project

---
+++ OnScreenUnitTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnScreenUnitTests
+//
+//  Created by Colin Reisterer on 1/11/19.
+//  Copyright © 2019 FullyTyped. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
